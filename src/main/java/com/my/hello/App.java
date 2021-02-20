444b9dc234a20770c13c5b35d10fa6e025684f1e
Kaixinguo <kaixinguo@kaixinguo.site>
2021-02-20 09:09:24 +0800

    Add feature: Jedis

--- src/main/java/com/my/hello/App.java
+++ src/main/java/com/my/hello/App.java
@@ -1,13 +1,21 @@
 package com.my.hello;
 
+import redis.clients.jedis.Jedis;
+
+import java.util.ResourceBundle;
+
 /**
- * Hello world!
- *
+ * Hello Redis!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App  {
+    public static void main( String[] args )  {
+        System.out.println( "Hello Redis!" );
+        ResourceBundle resource = ResourceBundle.getBundle("my-config");
+
+        Jedis jedis = new Jedis(resource.getString("redis.host"), Integer.parseInt(resource.getString("redis.port")));
+
+        System.out.print("Ping... ");
+        System.out.flush();
+        System.out.println(jedis.ping("OK"));
     }
 }
