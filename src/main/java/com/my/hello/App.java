bb7c465d8cd57e5cc7ed4ed053bbfe52d2b07a6b
Kaixinguo <kaixinguo@kaixinguo.site>
2021-02-20 10:16:29 +0800

    Add feature: Log4j2

--- src/main/java/com/my/hello/App.java
+++ src/main/java/com/my/hello/App.java
@@ -1,18 +1,32 @@
 package com.my.hello;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import redis.clients.jedis.Jedis;
 
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 /**
  * Hello Redis!
  */
 public class App  {
-    public static void main( String[] args )  {
-        System.out.println( "Hello Redis!" );
-        ResourceBundle resource = ResourceBundle.getBundle("my-config");
 
-        Jedis jedis = new Jedis(resource.getString("redis.host"), Integer.parseInt(resource.getString("redis.port")));
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args )  {
+
+        logger.info("Hello Redis!");
+
+        ResourceBundle resource = ResourceBundle.getBundle("my-config");
+        logger.debug(() -> "Current config file:\n" + resource.keySet().stream()
+                .map(key -> key + " = " + resource.getString(key))
+                .collect(Collectors.joining("\n")));
+
+        Jedis jedis = new Jedis(
+                resource.getString("redis.host"),
+                Integer.parseInt(resource.getString("redis.port"))
+        );
 
         System.out.print("Ping... ");
         System.out.flush();
